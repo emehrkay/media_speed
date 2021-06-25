26524b8f9830052743f4a6435b7bb6c23c86c4a0
emehrkay <mark.henderson@8trk.com>
2021-06-24 21:40:17 -0400

    added more controls

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ var ce = document.createElement.bind(document),
     select = ce('select'),
     range = ce('input'),
     rate = ce('span'),
-    cover = ce('div');
+    cover = ce('div'),
+    b_half = ce('a'),
+    b_one = ce('a'),
+    b_two = ce('a'),
+    b_three = ce('a'),
+    b_beg = ce('a'),
+    b_end = ce('a'),
+    buttons = ce('div');
 
 var choose = ce('option');
 choose.text = 'choose media';
@@ -65,6 +72,64 @@ if(media.length){
     range.value = media[0].playbackRate;
 }
 
+b_half.onclick = function(e){
+    e.preventDefault();
+    update(.5);
+    range.value = .5;
+}
+
+b_one.onclick = function(e){
+    e.preventDefault();
+    update(1);
+    range.value = 1;
+}
+
+b_two.onclick = function(e){
+    e.preventDefault();
+    update(2);
+    range.value = 2;
+}
+
+b_three.onclick = function(e){
+    e.preventDefault();
+    update(3);
+    range.value = 3;
+}
+
+b_beg.onclick = function(e){
+    e.preventDefault();
+    if(active){
+        active.currentTime = 1;
+    }
+}
+
+b_end.onclick = function(e){
+    e.preventDefault();
+    if(active){
+        active.currentTime = active.duration - 1;
+    }
+}
+
+buttons.appendChild(b_beg);
+buttons.appendChild(b_half);
+buttons.appendChild(b_one);
+buttons.appendChild(b_two);
+buttons.appendChild(b_three);
+buttons.appendChild(b_end);
+buttons.style.display = 'flex';
+buttons.style.justifyContent = 'space-between';
+b_half.innerHTML = '&frac12;x';
+b_half.href = '#';
+b_one.innerHTML = '1x';
+b_one.href = '#';
+b_two.innerHTML = '2x';
+b_two.href = '#';
+b_three.innerHTML = '3x';
+b_three.href = '#';
+b_beg.innerHTML = '<<';
+b_beg.href = '#';
+b_end.innerHTML = '>>';
+b_end.href = '#';
 range.type = 'range';
 range.min = '.25';
 range.max = '10';
@@ -89,6 +154,7 @@ cover.style.left = 0;
 cover.classList.add('video_speed_cover');
 container.appendChild(select);
 container.appendChild(range);
+container.appendChild(buttons);
 container.appendChild(rate);
 body.appendChild(cover);
 body.appendChild(container);
